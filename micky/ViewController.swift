cad560a409fe82bac1319ab4f22e8397a3e65955
Huang Yi-Chieh <huangyi-chieh@huangde-macbook-air.local>
2018-05-06 12:22:59 +0800

    modify

--- micky/ViewController.swift
+++ micky/ViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mickySlider: UIImageView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBAction func SliderChange(_ sender: Any) {
+              mickySlider.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(slider.value))
+    }
+    @IBAction func blueSliderChange(_ sender: Any) {
+              mickySlider.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(slider.value))
+    }
+    @IBAction func greenSliderChange(_ sender: Any) {
+              mickySlider.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(slider.value))
+    }
+    
+    @IBAction func redSliderChange(_ sender: Any) {
+        
+        mickySlider.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(slider.value))
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
